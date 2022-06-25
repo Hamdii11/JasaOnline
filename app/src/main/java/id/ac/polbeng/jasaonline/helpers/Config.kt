99483af9e64df24aea173c95cff740ee357f48d8
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-06-25 10:48:55 +0700

    jasa Online

---
+++ app/src/main/java/id/ac/polbeng/jasaonline/helpers/Config.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.jasaonline.helpers
+
+class Config {
+    companion object {
+        const val SPLASH_SCREEN_DELAY:Long = 5000
+        const val BASE_URL = "http://192.168.1.90:3000/"
+        const val EXTRA_FRAGMENT_ID = "FRAGMENT_ID"
+        const val IMAGE_URL = BASE_URL + "img/"
+        const val EXTRA_JASA = "SEND_JASA"
+    }
+}
